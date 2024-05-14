db6c0557f5b1a8f09148ccdd48cec415fe14700f
Dhavy Carvalho <davi.oaks@gmail.com>
2024-05-13 22:22:01 -0300

    Service layer, component registration

---
+++ src/main/java/com/neonoaksweb/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.neonoaksweb.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.neonoaksweb.course.entities.User;
+import com.neonoaksweb.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
